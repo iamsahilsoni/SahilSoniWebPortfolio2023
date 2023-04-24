96a6e6b7ded4a9f5ab4d2213e3f10558d61040b0
iamsahilsoni <sonisahil12345@gmail.com>
2023-04-24 14:47:08 -0400

    add react boilerplate for web portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>From Sahil Soni</h1>
+      <br />
+      <h2>
+        If this webpage is not updated until 2023-05-01, I will be chutiya no. 1
+      </h2>
+    </div>
+  );
+}
+
+export default App;
